b09b4dcdc25cef9ce692a06e6c95d0ce373d2d11
rgonzalezrdam1 <rgonzalezrdam1@gmail.com>
2020-10-24 16:52:55 +0200

    Tarea 3, Clases parametrizadas

--- Fabricante.cs
+++ Fabricante.cs
@@ -5,12 +5,18 @@ namespace fabricantevendedor
 {
     public class Fabricante
     {
+        private int _cantidad;
+
+        private int _periodicidad;
+
         private Almacen _a;
         private Thread _t;
         private Random _rnd = new Random();
-        public Fabricante(Almacen a)
+        public Fabricante(Almacen a, int cantidad, int periodicidad)
         {
             this._a = a;
+            this._cantidad = cantidad;
+            this._periodicidad = periodicidad;
         }
 
         public void Fabrica()
@@ -27,9 +33,9 @@ namespace fabricantevendedor
         private void _Accion()
         {
             int ms;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < _cantidad; i++)
             {
-                ms = _rnd.Next(1000, 2000);
+                ms = _periodicidad;
                 Thread.Sleep(ms);
                 _a.Guardar();
             }
